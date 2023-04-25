ed1438d95a97acdf8510314be7517e7bb549b487
Joon Hyuk Brandon Jang <joonbjang@gmail.com>
2023-04-24 17:20:08 -0700

    Manufacturer List is Complete.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import Manufacturers from './inventory/ManufacturerPage';
+import AutomobilesList from './services/Automobiles';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers/" element={<Manufacturers />} />
+          <Route path="automobiles/" element={<AutomobilesList />} />
         </Routes>
       </div>
     </BrowserRouter>
